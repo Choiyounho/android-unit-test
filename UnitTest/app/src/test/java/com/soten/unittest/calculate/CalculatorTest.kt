ccdbbafcbbba3ec43f44e46a318e11463d930135
Choiyounho <maxosa78@gmail.com>
2022-02-05 16:42:22 +0900

    [test] : 원의 둘레 구하기 테스트 코드 작성

--- UnitTest/app/src/test/java/com/soten/unittest/calculate/CalculatorTest.kt
+++ UnitTest/app/src/test/java/com/soten/unittest/calculate/CalculatorTest.kt
@@ -1,7 +1,22 @@
 package com.soten.unittest.calculate
 
-import org.junit.jupiter.api.Assertions.*
+import com.google.common.truth.Truth
+import org.junit.Before
+import org.junit.Test
 
-internal class CalculatorTest {
+class CalculatorTest {
+
+    private lateinit var calculator: Calculator
+
+    @Before
+    fun setUp() {
+        calculator = Calculator()
+    }
+
+    @Test
+    fun calculate_circumference_test() {
+        val result = calculator.calculateCircumference(2.1)
+        Truth.assertThat(result).isEqualTo(13.188)
+    }
 
 }
